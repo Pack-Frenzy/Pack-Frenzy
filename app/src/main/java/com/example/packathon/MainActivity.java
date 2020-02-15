faae06a1482c95d28ac91d6f6f89db36a998dc5b
Eric Kim <erickim195@gmail.com>
2020-02-15 14:20:51 -0800

    created PlayerActivity

--- app/src/main/java/com/example/packathon/MainActivity.java
+++ app/src/main/java/com/example/packathon/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // First page when app opens up
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Start Packing button -> PlayerActivity
+    // High Scores button -> Scoreboard Activity
 }
