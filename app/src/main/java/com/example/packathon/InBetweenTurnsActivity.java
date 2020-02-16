8fd792df0ffa5e5ddf75ad9f39497900a10c8f23
Scott <scott.m.king@outlook.com>
2020-02-15 20:14:52 -0800

    layout on round over page

--- app/src/main/java/com/example/packathon/InBetweenTurnsActivity.java
+++ app/src/main/java/com/example/packathon/InBetweenTurnsActivity.java
@@ -2,6 +2,7 @@ package com.example.packathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.TextView;
@@ -11,51 +12,43 @@ public class InBetweenTurnsActivity extends AppCompatActivity {
     private TextView countdownText;
     private CountDownTimer timer;
     private long timeLeftInMilliseconds = 10000; // 10 seconds
-    private boolean timeRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_over);
 
-        countdownText = findViewById(R.id.countdown_text);
+        countdownText = findViewById(R.id.countdownText);
         startTimer();
-
     }
 
     public void startTimer() {
         timer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
             @Override
-            public void onTick(long l) {
-                timeLeftInMilliseconds = 1;
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMilliseconds = millisUntilFinished;
                 updateTimer();
             }
 
             @Override
             public void onFinish() {
-
+                openTurnActivity();
             }
         }.start();
-
-        timeRunning = true;
-    }
-
-    public void stopTimer() {
-        timer.cancel();
-        timeRunning = false;
     }
 
     public void updateTimer() {
         int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
+        int minutes = 0;
 
-        String timeLeftText = "";
-
-        if (seconds < 10) timeLeftText += "0";
+        String timeLeftText = String.format("%02d:%02d", minutes, seconds);
 
         countdownText.setText(timeLeftText);
     }
 
+    public void openTurnActivity() {
+        Intent intent = new Intent(this, TurnActivity.class);
+        startActivity(intent);
+    }
 
-    // TODO:
-    // Countdown from 10 -> TurnActivity
 }
