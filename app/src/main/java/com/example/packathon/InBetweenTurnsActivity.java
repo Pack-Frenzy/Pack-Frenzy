9ff7dd5915506ac1501df0af4069840d54d55886
Scott <scott.m.king@outlook.com>
2020-02-15 19:30:36 -0800

    Frontend for in between turn

--- app/src/main/java/com/example/packathon/InBetweenTurnsActivity.java
+++ app/src/main/java/com/example/packathon/InBetweenTurnsActivity.java
@@ -3,14 +3,58 @@ package com.example.packathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class InBetweenTurnsActivity extends AppCompatActivity {
+
+    private TextView countdownText;
+    private CountDownTimer timer;
+    private long timeLeftInMilliseconds = 10000; // 10 seconds
+    private boolean timeRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_over);
+
+        countdownText = findViewById(R.id.countdown_text);
+        startTimer();
+
     }
+
+    public void startTimer() {
+        timer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilliseconds = 1;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        timeRunning = true;
+    }
+
+    public void stopTimer() {
+        timer.cancel();
+        timeRunning = false;
+    }
+
+    public void updateTimer() {
+        int seconds = (int) timeLeftInMilliseconds % 60000 / 1000;
+
+        String timeLeftText = "";
+
+        if (seconds < 10) timeLeftText += "0";
+
+        countdownText.setText(timeLeftText);
+    }
+
 
     // TODO:
     // Countdown from 10 -> TurnActivity
--- app/src/main/java/com/example/packathon/ScoreboardActivity.java
+++ app/src/main/java/com/example/packathon/ScoreboardActivity.java
@@ -6,6 +6,8 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ScoreboardActivity extends AppCompatActivity {
 
@@ -13,6 +15,20 @@ public class ScoreboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+
+        Button home = findViewById(R.id.homeButton);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     // TODO:
