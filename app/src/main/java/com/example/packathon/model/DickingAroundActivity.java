df4442d6c83a662fc0e86217b902975a4529931f
Eric Kim <erickim195@gmail.com>
2020-02-23 04:53:21 -0800

    added DickingAroundActivity for experimenting

---
+++ app/src/main/java/com/example/packathon/model/DickingAroundActivity.java
@@ -0,0 +1,14 @@
+package com.example.packathon.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DickingAroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dicking_around);
+    }
+}
