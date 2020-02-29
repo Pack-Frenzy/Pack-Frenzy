049208c64c57b4fe7cb5e7521c46a65572564b4b
Scott <scott.m.king@outlook.com>
2020-02-29 00:44:13 -0800

    Got pause screen working

--- app/src/main/java/com/example/packathon/PauseScreenActivity.java
+++ app/src/main/java/com/example/packathon/PauseScreenActivity.java
@@ -2,9 +2,10 @@ package com.example.packathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class PauseScreenActivity extends AppCompatActivity {
+public class PauseScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
