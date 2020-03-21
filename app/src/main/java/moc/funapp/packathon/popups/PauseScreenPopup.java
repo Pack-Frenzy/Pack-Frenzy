568a74272cfffc3e5290d69ce9f745dd99a51297
Eric Kim <erickim195@gmail.com>
2020-03-20 21:02:57 -0700

    minor refactor

--- app/src/main/java/moc/funapp/packathon/popups/PauseScreenPopup.java
+++ app/src/main/java/moc/funapp/packathon/popups/PauseScreenPopup.java
@@ -15,6 +15,5 @@ public class PauseScreenPopup extends Activity {
         // TODO: Switch to proper pause screen below:
         // setContentView(R.layout.activity_pause_screen);
         setContentView(R.layout.activity_pause_screen_old);
-
     }
 }
